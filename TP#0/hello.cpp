d12b5d220d43493d84f2d57c663d93cf5735cab8
pablocactus <avendano.pablo@gmail.com>
2015-04-23 17:28:55 -0300

    TP#0

---
+++ TP#0/hello.cpp
@@ -0,0 +1,19 @@
+/* 
+* TP #0 Hola Mundo
+
+* Martin Ribola, Cynthia Natalia Soledad 
+
+* 23/04/15
+
+*/
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout<<"hola mundo";
+    return EXIT_SUCCESS;
+}
